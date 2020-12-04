22242451288c0aed2a55b8686641ddcd8fee7cdc
jeanyvesruffin <ruffinmax@hotmail.fr>
2020-12-04 09:44:03 +0100

    init projet

---
+++ src/main/java/com/ruffin/JeeSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package com.ruffin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeSpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeeSpringAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ruffin/JeeSpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruffin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeSpringAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
